7f99517e388820c2c3247bbae7601efbaeeb81a1
uunw <taokaeg2@gmail.com>
2022-01-20 23:23:09 +0700

    remove last source

--- events/client/shardReady.js
+++
@@ -1,5 +0,0 @@
-const chalk = require("chalk");
-
-module.exports = async(client, id) => {
-    console.log(chalk.green(`[SYSTEM] (${String(new Date).split(" ", 5).join(" ")}) Shard ${id} Shard ready!`));
-}
