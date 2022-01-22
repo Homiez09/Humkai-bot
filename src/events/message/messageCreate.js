3e76ae22af0e4530159d5cede50b2201af995787
Phumrapee Soenvanichakul <phumsoen123@gmail.com>
2022-01-22 15:12:46 +0700

    Update

--- src/events/message/messageCreate.js
+++ src/events/message/messageCreate.js
@@ -13,10 +13,10 @@ module.exports = async (client, msg) => {
     }
   }
 
-  if (msg.channel.id == '881090796319813642') { // ใส่ Channel Id สำหรับ input รูปภาพ
-    msg.attachments.forEach(attachment => {
+  if (msg.channel.id == '881090796319813642') {
+    // ใส่ Channel Id สำหรับ input รูปภาพ
+    msg.attachments.forEach((attachment) => {
       const ImageLink = attachment.proxyURL;
-
       const formData = new FormData();
       formData.append('size', 'auto');
       formData.append('image_url', ImageLink);
@@ -30,24 +30,34 @@ module.exports = async (client, msg) => {
           ...formData.getHeaders(),
           'X-Api-Key': process.env.RMBG_API,
         },
-        encoding: null
+        encoding: null,
       })
-      .then((response) => {
-        if(response.status != 200) return console.error('Error:', response.status, response.statusText);
-        fs.writeFileSync(`./src/assets/images/transparent/${msg.author.id}.png`, response.data);
-        //console.log('Saved.');
-        let attachment1 = new MessageAttachment(`./src/assets/images/transparent/${msg.author.id}.png`)
-        client.channels.cache
-          .get('881090831119970314') // ใส่ Channel Id สำหรับ output รูปภาพ
-          .send({ content: `<@${msg.author.id}> ลบพื้นหลังเรียบร้อย`, files: [attachment1] });
-      })
-      .catch((error) => {    
+        .then((response) => {
+          if (response.status != 200)
+            return console.error(
+              'Error:',
+              response.status,
+              response.statusText,
+            );
+          fs.writeFileSync(
+            `./src/assets/images/transparent/${msg.author.id}.png`,
+            response.data,
+          );
+          //console.log('Saved.');
+          let attachment1 = new MessageAttachment(
+            `./src/assets/images/transparent/${msg.author.id}.png`,
+          );
           client.channels.cache
-          .get(process.env.BOT_CHANNEL_ID) // ใส่ Channel Id สำหรับ output รูปภาพ
-          .send({ content: `<@${msg.author.id}> ลบพื้นหลังเรียบร้อย`, files: [attachment1] });
+            .get('881090831119970314') // ใส่ Channel Id สำหรับ output รูปภาพ
+            .send({
+              content: `<@${msg.author.id}> ลบพื้นหลังเรียบร้อย`,
+              files: [attachment1],
+            });
+        })
+        .catch((error) => {
+          msg.reply('ยังไม่รองรับไฟล์นามสกุล .webp')
           return console.error('Request failed:', error);
-      });
+        });
     });
   }
 };
-  
