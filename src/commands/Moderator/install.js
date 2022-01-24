aa9b102f97847ee2923d2b24d0d9dff5f699f359
Phumrapee Soenvanichakul <phumsoen123@gmail.com>
2022-01-25 02:38:16 +0700

    Update

---
+++ src/commands/Moderator/install.js
@@ -0,0 +1,129 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: 'install',
+  description: 'ติดตั้ง',
+  userPerms: ['MANAGE_MESSAGES'],
+  options: [
+    {
+      name: "options",
+      description: "ตัวเลือก",
+      type: 3,
+      required: true,
+      choices:[
+        {
+          name: 'all',
+          value: 'all',
+        },
+        {
+          name: 'voice',
+          value: 'voice',
+        },
+        {
+          name: 'remove-background',
+          value: 'remove-background',
+        }
+      ]
+    },
+  ],
+  run: async (interaction, client) => {
+    const { options } = interaction;
+    const choice = options.getString('options');
+    switch(choice) {
+      case 'all':
+        {
+          let category_voice = await interaction.guild.channels.create('voice by humkai', {
+            type: 'GUILD_CATEGORY',
+          });
+          
+          let category_rebg = await interaction.guild.channels.create('rebg by humkai', {
+            type: 'GUILD_CATEGORY',
+          });
+      
+          let channel0 = await interaction.guild.channels.create('Create New Room', {
+            type: 'GUILD_VOICE',
+            parent: category_voice
+          });
+
+          let channel1 = await interaction.guild.channels.create('input-image', {
+            type: 'GUILD_TEXT',
+            parent: category_rebg
+          });
+
+          let channel2 = await interaction.guild.channels.create('output-image', {
+            type: 'GUILD_TEXT',
+            parent: category_rebg
+          });
+          return interaction.reply({
+            embeds: [
+              await new MessageEmbed()
+                .setTitle('Installed')
+                .addField('Create Voice Channel', `คลิกที่ <#${channel0.id}> เพื่อสร้างห้องของตัวเอง `)
+                .addField('Remove Background', `ส่งรูปภาพไปที่ <#${channel1.id}> แล้วรับภาพที่ <#${channel2.id}>`)
+                .setColor('#0099ff')
+                .setFooter(
+                  `Requested by ${interaction.user.tag}`,
+                  interaction.user.displayAvatarURL(),
+                ),
+            ],
+          });
+        }
+        break;
+
+      case 'voice':
+        {
+          let category_voice = await interaction.guild.channels.create('voice by humkai', {
+            type: 'GUILD_CATEGORY',
+          });
+      
+          let channel0 = await interaction.guild.channels.create('Create New Room', {
+            type: 'GUILD_VOICE',
+            parent: category_voice
+          });
+          return interaction.reply({
+            embeds: [
+              await new MessageEmbed()
+                .setTitle('Installed')
+                .addField('Create Voice Channel', `คลิกที่ <#${channel0.id}> เพื่อสร้างห้องของตัวเอง `)
+                .setColor('#0099ff')
+                .setFooter(
+                  `Requested by ${interaction.user.tag}`,
+                  interaction.user.displayAvatarURL(),
+                ),
+            ],
+          });     
+        }
+        break;
+      case 'remove-background':
+        {
+          {
+            let category_rebg = await interaction.guild.channels.create('rebg by humkai', {
+              type: 'GUILD_CATEGORY',
+            });
+
+            let channel1 = await interaction.guild.channels.create('input-image', {
+              type: 'GUILD_TEXT',
+              parent: category_rebg
+            });
+            let channel2 = await interaction.guild.channels.create('output-image', {
+              type: 'GUILD_TEXT',
+              parent: category_rebg
+            });
+            return interaction.reply({
+              embeds: [
+                await new MessageEmbed()
+                  .setTitle('Installed')
+                  .addField('Remove Background', `ส่งรูปภาพไปที่ <#${channel1.id}> แล้วรับภาพที่ <#${channel2.id}>`)
+                  .setColor('#0099ff')
+                  .setFooter(
+                    `Requested by ${interaction.user.tag}`,
+                    interaction.user.displayAvatarURL(),
+                  ),
+              ],
+            });
+          }
+        }
+        break;
+    }
+  },
+};
--- src/events/message/messageCreate.js
+++ src/events/message/messageCreate.js
@@ -15,57 +15,64 @@ module.exports = async (client, msg) => {
   }
 
   /* ลบพื้นหลัง */
-  // ใส่ Channel Id สำหรับ input รูปภาพ
-  if (msg.channel.id == '881090796319813642') {
-    msg.attachments.forEach((attachment) => {
-      // ดึง Url จากรูปภาพที่ user ส่ง
-      const ImageLink = attachment.proxyURL;
-      // สร้างตัวแปรเพื่อเก็บนามสกุลของไฟล์ภาพ เช่น [png, jpg]
-      const FileExt = ImageLink.split('.')[-1];
-      const formData = new FormData();
-      formData.append('size', 'auto');
-      formData.append('image_url', ImageLink);
-
-      axios({
-        method: 'post',
-        url: 'https://api.remove.bg/v1.0/removebg',
-        data: formData,
-        responseType: 'arraybuffer',
-        headers: {
-          ...formData.getHeaders(),
-          'X-Api-Key': process.env.RMBG_API,
-        },
-        encoding: null,
-      })
-        .then((response) => {
-          if (response.status != 200)
-            return console.error(
-              'Error:',
-              response.status,
-              response.statusText,
-            );
-          // บันทึกรูปภาพที่เปลี่ยนพื้นหลังไว้ที่โฟลเดอร์
-          fs.writeFileSync(
-            `./src/assets/images/transparent/${msg.author.id}.png`,
-            response.data,
-          );
-          //console.log('Saved.');
-          // สร้างตัวแปร attachmemt1 เพื่อกำหนดที่อยู่ของรูปภาพที่จะส่งไปยังช่องแชท
-          let attachment1 = new MessageAttachment(
-            `./src/assets/images/transparent/${msg.author.id}.png`,
-          );
-          client.channels.cache
-            .get('881090831119970314') // ใส่ Channel Id สำหรับ output รูปภาพ
-            .send({
-              content: `<@${msg.author.id}> ลบพื้นหลังเรียบร้อย`,
-              files: [attachment1],
-            });
+  try {
+    // รับค่า Channel ID ที่ input ไฟล์ภาพเข้ามา
+    const input_image = msg.guild.channels.cache.find(channel => channel.name === 'input-image').id;
+    // รับค่า Channel ID ที่ input ไฟล์ภาพเข้ามา
+    const output_image = msg.guild.channels.cache.find(channel => channel.name === 'output-image').id;
+    if (msg.channel.id == input_image) {
+      msg.attachments.forEach((attachment) => {
+        // ดึง Url จากรูปภาพที่ user ส่ง
+        const ImageLink = attachment.proxyURL;
+        // สร้างตัวแปรเพื่อเก็บนามสกุลของไฟล์ภาพ เช่น [png, jpg]
+        const FileExt = ImageLink.split('.')[-1];
+        const formData = new FormData();
+        formData.append('size', 'auto');
+        formData.append('image_url', ImageLink);
+  
+        axios({
+          method: 'post',
+          url: 'https://api.remove.bg/v1.0/removebg',
+          data: formData,
+          responseType: 'arraybuffer',
+          headers: {
+            ...formData.getHeaders(),
+            'X-Api-Key': process.env.RMBG_API,
+          },
+          encoding: null,
         })
-        // กรณีที่เว็บรองรับไฟล์นามสกุลนั้นๆ
-        .catch((error) => {
-          msg.reply(`ยังไม่รองรับไฟล์นามสกุล ${FileExt}`);
-          return console.error('Request failed:', error);
-        });
-    });
+          .then((response) => {
+            if (response.status != 200)
+              return console.error(
+                'Error:',
+                response.status,
+                response.statusText,
+              );
+            // บันทึกรูปภาพที่เปลี่ยนพื้นหลังไว้ที่โฟลเดอร์
+            fs.writeFileSync(
+              `./src/assets/images/transparent/${msg.author.id}.png`,
+              response.data,
+            );
+            //console.log('Saved.');
+            // สร้างตัวแปร attachmemt1 เพื่อกำหนดที่อยู่ของรูปภาพที่จะส่งไปยังช่องแชท
+            let attachment1 = new MessageAttachment(
+              `./src/assets/images/transparent/${msg.author.id}.png`,
+            );
+            client.channels.cache
+              .get(output_image) // ใส่ Channel Id สำหรับ output รูปภาพ
+              .send({
+                content: `<@${msg.author.id}> ลบพื้นหลังเรียบร้อย`,
+                files: [attachment1],
+              });
+          })
+          // กรณีที่เว็บไม่รองรับไฟล์นามสกุลนั้นๆ
+          .catch((error) => {
+            msg.reply(`ยังไม่รองรับไฟล์นามสกุล ${FileExt}`);
+            return console.error('Request failed:', error);
+          });
+      });
+    }   
+  } catch (error) {
+    return;
   }
 };
