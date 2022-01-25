eb4f699bf6914475503e20a31fa3cf30bcaee2af
Phumrapee Soenvanichakul <77825705+Homiez09@users.noreply.github.com>
2022-01-25 12:25:08 +0700

    Update

--- src/commands/Authentication/auth.js
+++ src/commands/Authentication/auth.js
@@ -3,13 +3,17 @@ module.exports = {
   description: 'ยืนยันตัวตน',
   run: async (interaction, client) => {
     const role = '871634427552358421';
-    await interaction.member.roles.add(role);
-    client.channels.cache
-      .get('882234093495017513')
-      .send(`ยินดีด้วยคุณ <@${interaction.user.id}> ยืนยันตัวตนเรียบร้อย`);
-    interaction.reply({
-      content: `คุณเป็นสมาชิกเรียบร้อยแล้ว`,
-      ephemeral: true,
-    });
+    try {
+      await interaction.member.roles.add(role);
+      client.channels.cache
+        .get('882234093495017513')
+        .send(`ยินดีด้วยคุณ <@${interaction.user.id}> ยืนยันตัวตนเรียบร้อย`);
+      interaction.reply({
+        content: `คุณเป็นสมาชิกเรียบร้อยแล้ว`,
+        ephemeral: true,
+      });    
+    } catch (error) {
+      
+    }
   },
 };
