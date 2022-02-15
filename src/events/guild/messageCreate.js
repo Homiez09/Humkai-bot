da361266df86b0183ec401b35bc410bb30031353
Phumrapee Soenvanichakul <77825705+Homiez09@users.noreply.github.com>
2022-02-15 20:38:16 +0700

    update

--- src/events/guild/messageCreate.js
+++ src/events/guild/messageCreate.js
@@ -3,59 +3,95 @@ const { MessageAttachment } = require('discord.js');
 const axios = require('axios');
 const FormData = require('form-data');
 const channelModel = require('../../schemas/channelDB');
+const rankModel = require('../../schemas/rankDB');
 
 require('dotenv').config();
 
 module.exports = async (client, msg) => {
-  /* Remove Background */
-  try {
-    const channelData = await channelModel.findOne({ guild_ID: msg.guild.id });
-    const input_image = channelData.remove_ID;
-    const bot_id = msg.guild.members.cache.get(client.user.id).id;
-    if (msg.channel.id == input_image && msg.author.id != bot_id) {
-      msg.attachments.forEach((attachment) => {
-        const ImageLink = attachment.proxyURL;
-        const formData = new FormData();
-        formData.append('size', 'auto');
-        formData.append('image_url', ImageLink);
+  /* increase your rank when typing*/
+  if (!msg.author.bot) {
+    try {
+      let rankData;
+      rankData = await rankModel.findOneAndUpdate(
+        {
+          userID: msg.author.id
+        },
+        {
+          $inc: { point: 1 }
+        });
+      if (!rankData) {
+        rankData = await rankModel.create({
+          userID: msg.author.id,
+        });
+      }
 
-        axios({
-          method: 'post',
-          url: 'https://api.remove.bg/v1.0/removebg',
-          data: formData,
-          responseType: 'arraybuffer',
-          headers: {
-            ...formData.getHeaders(),
-            'X-Api-Key': process.env.RMBG_API,
+      if ((rankData.point + 1) >= Math.pow(rankData.rank, 4)) {
+        rankData = await rankModel.findOneAndUpdate(
+          {
+            userID: msg.author.id
           },
-          encoding: null,
-        })
-          .then((response) => {
-            if (response.status != 200)
-              return console.error(
-                'Error:',
-                response.status,
-                response.statusText,
-              );
-            let attachment1 = new MessageAttachment(response.data);
-            msg.reply({
-              content: `<@${msg.author.id}> ลบพื้นหลังเรียบร้อย`,
-              files: [attachment1],
-            });
-          })
-          .catch((error) => {
-            if (error.response.status == 402) {
-              msg.reply(
-                `CODE:${error.response.status} การใช้งานในเดือนนี้สิ้นสุดแล้ว`,
-              );
-            } else if (error.response.status == 400) {
-              msg.reply(`CODE:${error.response.status} อัพโหลดไฟล์ไม่ถูกต้อง`);
-            }
-            return console.error('Request failed:', error);
+          {
+            $inc: { rank: 1 }
           });
-      });
+        msg.reply(`คุณเพิ่งไปถึงระดับ ${rankData.rank}`)
+      }
     }
-  } catch (error) {
-    return;
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  /* Remove Background */
+  if (!msg.author.bot) {
+    try {
+      const channelData = await channelModel.findOne({ guild_ID: msg.guild.id });
+      const input_image = channelData.remove_ID;
+      const bot_id = msg.guild.members.cache.get(client.user.id).id;
+      if (msg.channel.id == input_image && msg.author.id != bot_id) {
+        msg.attachments.forEach((attachment) => {
+          const ImageLink = attachment.proxyURL;
+          const formData = new FormData();
+          formData.append('size', 'auto');
+          formData.append('image_url', ImageLink);
+
+          axios({
+            method: 'post',
+            url: 'https://api.remove.bg/v1.0/removebg',
+            data: formData,
+            responseType: 'arraybuffer',
+            headers: {
+              ...formData.getHeaders(),
+              'X-Api-Key': process.env.RMBG_API,
+            },
+            encoding: null,
+          })
+            .then((response) => {
+              if (response.status != 200)
+                return console.error(
+                  'Error:',
+                  response.status,
+                  response.statusText,
+                );
+              let attachment1 = new MessageAttachment(response.data);
+              msg.reply({
+                content: `<@${msg.author.id}> ลบพื้นหลังเรียบร้อย`,
+                files: [attachment1],
+              });
+            })
+            .catch((error) => {
+              if (error.response.status == 402) {
+                msg.reply(
+                  `CODE:${error.response.status} การใช้งานในเดือนนี้สิ้นสุดแล้ว`,
+                );
+              } else if (error.response.status == 400) {
+                msg.reply(`CODE:${error.response.status} อัพโหลดไฟล์ไม่ถูกต้อง`);
+              }
+              return console.error('Request failed:', error);
+            });
+        });
+      }
+    } catch (error) {
+      return;
+    }
   }
 };
---
+++ src/schemas/rankDB.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const rankSchema = new mongoose.Schema({
+    userID: { type: String, required: true, unique: true },
+    rank: { type: Number, default: 1 },
+    point: { type: Number, default: 0 },
+});
+
+const model = mongoose.model('rankModels', rankSchema);
+
+module.exports = model;
