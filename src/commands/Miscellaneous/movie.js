7441fc276d63d0f3edd6739e9cce88e38d9cba04
Phumrapee Soenvanichakul <phumsoen123@gmail.com>
2022-04-14 04:45:20 +0700

    Downgrade | cheerio module

--- src/commands/Miscellaneous/movie.js
+++ src/commands/Miscellaneous/movie.js
@@ -3,118 +3,117 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 module.exports = {
-    name: 'movie',
-    description: 'โปรแกรมฉายหนัง',
-    category: 'miscellaneous',
-    botPerms: ['ADMINISTRATOR'],
-    options: [
+  name: 'movie',
+  description: 'โปรแกรมฉายหนัง',
+  category: 'miscellaneous',
+  botPerms: ['ADMINISTRATOR'],
+  options: [
+    {
+      name: 'options',
+      description: 'ตัวเลือก',
+      type: 3,
+      required: true,
+      choices: [
         {
-            name: 'options',
-            description: 'ตัวเลือก',
-            type: 3,
-            required: true,
-            choices: [
-                {
-                    name: 'major',
-                    value: 'major',
-                },
-                {
-                    name: 'sf',
-                    value: 'sf',
-                },
-            ],
+          name: 'major',
+          value: 'major',
         },
-    ],
-    run: async (interaction, client, word) => {
-        const { options } = interaction;
-        const choice = options.getString('options');
-        switch (choice) {
-            case 'major':
-                {
-                    try {
-                        const siteUrl = 'https://majorcineplex.com/movie#movie-page-coming';
+        {
+          name: 'sf',
+          value: 'sf',
+        },
+      ],
+    },
+  ],
+  run: async (interaction, client, word) => {
+    const { options } = interaction;
+    const choice = options.getString('options');
+    switch (choice) {
+      case 'major':
+        {
+          try {
+            const siteUrl = 'https://majorcineplex.com/movie#movie-page-coming';
 
-                        const { data } = await axios({
-                            method: "GET",
-                            url: siteUrl,
-                        })
+            const { data } = await axios({
+              method: 'GET',
+              url: siteUrl,
+            });
 
-                        const $ = cheerio.load(data);
-                        const elemSelector = '#movie-page-showing > div > div'
+            const $ = cheerio.load(data);
+            const elemSelector = '#movie-page-showing > div > div';
 
-                        const keys = [
-                            'genre',
-                            'date',
-                            'name',
-                            'time',
-                        ]
+            const keys = ['genre', 'date', 'name', 'time'];
 
-                        var listA = []
-                        $(elemSelector).each((parentIdx, parentElem) => {
-                            let keyIdx = 0;
-                            const jsonList = {}
+            var listA = [];
+            $(elemSelector).each((parentIdx, parentElem) => {
+              let keyIdx = 0;
+              const jsonList = {};
 
-                            if (true) {
-                                $(parentElem).children().each((childIdx, childElem) => {
-                                    const tdValue = $(childElem).text();
-                                    const tdValueOptimized = tdValue.replace(/\s\s+ /g, ' ').trim();
-                                    const tdValueArray = tdValueOptimized.split(/\n/);
-                                    jsonList[keys[keyIdx]] = tdValueArray
-                                    //jsonList[keys[keyIdx]] = tdValueArray;
-                                    keyIdx++;
+              if (true) {
+                $(parentElem)
+                  .children()
+                  .each((childIdx, childElem) => {
+                    const tdValue = $(childElem).text();
+                    const tdValueOptimized = tdValue
+                      .replace(/\s\s+ /g, ' ')
+                      .trim();
+                    const tdValueArray = tdValueOptimized.split(/\n/);
+                    jsonList[keys[keyIdx]] = tdValueArray;
+                    //jsonList[keys[keyIdx]] = tdValueArray;
+                    keyIdx++;
+                  });
+                listA.push(jsonList);
+              }
+            });
+            //return console.log(listA);
+            lengthOfListA = listA.length;
 
-                                })
-                                listA.push(jsonList);
-                            }
-                        })
-                        //return console.log(listA);
-                        lengthOfListA = listA.length;
+            const embed = new MessageEmbed()
+              .setTitle(
+                `หนังที่กำลังฉาย (Major) | ทั้งหมด ${lengthOfListA} รายการ`,
+              )
+              .setDescription(
+                '[ข้อมูลจาก MajorCineplex](https://majorcineplex.com/movie)',
+              )
+              .setColor('#0099ff')
+              .setTimestamp()
+              .setFooter(
+                `Requested by ${interaction.user.tag}`,
+                interaction.user.displayAvatarURL(),
+              );
 
-                        const embed = new MessageEmbed()
-                            .setTitle(`หนังที่กำลังฉาย (Major) | ทั้งหมด ${lengthOfListA} รายการ`)
-                            .setDescription("[ข้อมูลจาก MajorCineplex](https://majorcineplex.com/movie)")
-                            .setColor('#0099ff')
-                            .setTimestamp()
-                            .setFooter(
-                                `Requested by ${interaction.user.tag}`,
-                                interaction.user.displayAvatarURL(),
-                            );
+            for (let i = 0; i < lengthOfListA; i++) {
+              embed.addField(`${listA[i].name[0]}`, `${listA[i].time[0]}`);
+            }
 
-                        for (let i = 0; i < lengthOfListA; i++) {
-                            embed.addField(
-                                `${listA[i].name[0]}`,
-                                `${listA[i].time[0]}`,
-                            )
-                        }
+            await interaction.reply({ embeds: [embed], ephemeral: false });
+          } catch (error) {
+            console.log(error);
+          }
+        }
+        break;
+      case 'sf':
+        {
+          const embed = new MessageEmbed()
+            .setTitle(`หนังที่กำลังฉาย (SF) | ทั้งหมด X รายการ`)
+            .setDescription('ยังไม่ได้ทำ ขีเกียจ')
+            .setColor('#0099ff')
+            .setTimestamp()
+            .setFooter(
+              `Requested by ${interaction.user.tag}`,
+              interaction.user.displayAvatarURL(),
+            );
 
-                        await interaction.reply({ embeds: [embed], ephemeral: false });
-                    } catch (error) {
-                        console.log(error);
-                    }
-                }
-                break;
-            case 'sf':
-                {
-                    const embed = new MessageEmbed()
-                        .setTitle(`หนังที่กำลังฉาย (SF) | ทั้งหมด X รายการ`)
-                        .setDescription("ยังไม่ได้ทำ ขีเกียจ")
-                        .setColor('#0099ff')
-                        .setTimestamp()
-                        .setFooter(
-                            `Requested by ${interaction.user.tag}`,
-                            interaction.user.displayAvatarURL(),
-                        );
-
-                    /* for (let i = 0; i < lengthOfListA; i++) {
+          /* for (let i = 0; i < lengthOfListA; i++) {
                         embed.addField(
                             `${listA[i].name[0]}`,
                             `${listA[i].time[0]}`,
                         )
                     } */
 
-                    await interaction.reply({ embeds: [embed], ephemeral: false });
-                }
-                break;
+          await interaction.reply({ embeds: [embed], ephemeral: false });
         }
-    },
+        break;
+    }
+  },
 };
