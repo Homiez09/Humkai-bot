312e81b6669c2bdc26ec7bddc27df430eb06b003
Phumrapee Soenvanichakul <phumsoen123@gmail.com>
2022-06-11 16:19:25 +0700

    update 2.11.10

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -18,6 +18,7 @@ module.exports = async (client) => {
         .map((guild) => guild.memberCount)
         .reduce((a, b) => a + b, 0)} users`,
       `${client.guilds.cache.size} servers`,
+      '/setup'
     ];
 
     client.user.setPresence({
@@ -33,5 +34,5 @@ module.exports = async (client) => {
     });
 
     i++;
-  }, 10000);
+  }, 5000);
 };
